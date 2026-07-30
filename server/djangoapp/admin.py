d990bb4af1d5814d94ccdf66134f6eea4232d03c
Gautam <gautamkumar09630@gmail.com>
2026-07-30 05:02:00 -0400

    Add login/logout/registration views, CarMake/CarModel with populate script, and getallcarmakes output

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,17 @@
-# from django.contrib import admin
-# from .models import related models
-
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # Register your models here.
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
 
-# CarModelInline class
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ["name", "description"]
 
-# CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "car_make", "type", "year"]
 
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,2 +1,35 @@
 def initiate():
-    print("Populate not implemented. Add data manually")
+    from .models import CarMake, CarModel
+
+    car_make_data = [
+        {"name": "Toyota", "description": "Reliable Japanese car maker known for quality and durability."},
+        {"name": "Ford", "description": "American automobile manufacturer known for trucks and SUVs."},
+        {"name": "BMW", "description": "German luxury vehicle manufacturer known for performance."},
+        {"name": "Honda", "description": "Japanese manufacturer known for fuel efficient cars."},
+    ]
+
+    car_make_objs = []
+    for data in car_make_data:
+        car_make_obj, created = CarMake.objects.get_or_create(
+            name=data["name"], description=data["description"]
+        )
+        car_make_objs.append(car_make_obj)
+
+    carmodels_data = [
+        {"car_make": car_make_objs[0], "name": "Corolla", "type": "SEDAN", "year": 2022},
+        {"car_make": car_make_objs[0], "name": "RAV4", "type": "SUV", "year": 2023},
+        {"car_make": car_make_objs[1], "name": "F-150", "type": "TRUCK", "year": 2022},
+        {"car_make": car_make_objs[1], "name": "Explorer", "type": "SUV", "year": 2021},
+        {"car_make": car_make_objs[2], "name": "X5", "type": "SUV", "year": 2023},
+        {"car_make": car_make_objs[2], "name": "3 Series", "type": "SEDAN", "year": 2022},
+        {"car_make": car_make_objs[3], "name": "Civic", "type": "SEDAN", "year": 2021},
+        {"car_make": car_make_objs[3], "name": "CR-V", "type": "SUV", "year": 2023},
+    ]
+
+    for data in carmodels_data:
+        CarModel.objects.get_or_create(
+            car_make=data["car_make"], name=data["name"],
+            type=data["type"], year=data["year"]
+        )
+
+    print("Car makes and models populated successfully.")
